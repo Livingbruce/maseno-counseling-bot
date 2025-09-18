de46c59671d70ea45a0433e0300651df119c546e
Livingbruce <vicymbrush@gmail.com>
2025-09-18 20:29:29 +0300

    Convert to ES modules for Vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -66,4 +66,4 @@ module.exports = async (req, res) => {
       details: error.message
     });
   }
-};
+}
